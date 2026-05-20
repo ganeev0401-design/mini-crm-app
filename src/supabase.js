ea72c75f0f12ac182500018b745430157ad305f8
Artem Ganeev <artemganeev@MacBook-Pro-od-Artem.local>
2026-05-20 21:57:53 +0500

    fix mini app crash

--- src/supabase.js
+++ src/supabase.js
@@ -3,6 +3,6 @@ import { createClient } from "@supabase/supabase-js"
 console.log("ENV CHECK:", process.env.SUPABASE_URL)
 
 export const supabase = createClient(
-  "https://fofhpbpbixdghqbjpyha.supabase.co",
+  console.log("SUPABASE URL:", "https://fofhpbpbixdghqbjpyha.supabase.co"),
   "sb_publishable_6Ge6fb3UBvKZ4dNBaBVVUA_ZxYpGr1M"
 )
