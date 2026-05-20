7690e23428922fae7faf637c67951fc9846de6ec
Artem Ganeev <artemganeev@MacBook-Pro-od-Artem.local>
2026-05-20 22:05:15 +0500

    update

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-console.log("ENV CHECK:", process.env.SUPABASE_URL)
-
 export const supabase = createClient(
-  console.log("SUPABASE URL:", "https://fofhpbpbixdghqbjpyha.supabase.co"),
-  "sb_publishable_6Ge6fb3UBvKZ4dNBaBVVUA_ZxYpGr1M"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
